dc613ce2fdad3cdc4cfe90e446e5cd899a876606
Serhii <smolanovsergey@gmail.com>
2023-08-21 11:42:56 +0300

    first

--- wp-content/themes/subareva-v2/page.php
+++ wp-content/themes/subareva-v2/page.php
@@ -17,11 +17,11 @@ get_header();
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '308498198376081');
+        fbq('init', '676280260679080');
         fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=308498198376081&ev=PageView&noscript=1"
+                   src="https://www.facebook.com/tr?id=676280260679080&ev=PageView&noscript=1"
         /></noscript>
     <!-- End Meta Pixel Code -->
     <main id="main" class="main">
