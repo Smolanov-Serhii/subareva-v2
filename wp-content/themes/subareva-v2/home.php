84dbb25e86039b561522d9495c33cd45b7df5291
Сергей <smolanovsergey@gmail.com>
2024-03-01 07:42:43 +0200

    first

--- wp-content/themes/subareva-v2/home.php
+++ wp-content/themes/subareva-v2/home.php
@@ -25,7 +25,62 @@ get_header();
 <!--        /></noscript>-->
     <!-- End Meta Pixel Code -->
     <main id="main" class="main">
-        <?php get_template_part( 'template-parts/content', 'banner-main' ); ?>
+        <section class="banner-dark white">
+            <div class="banner-dark__img">
+
+                <?php
+                $currentLang = $GLOBALS['q_config']['language'];
+                if ( wp_is_mobile() ) {
+                    ?>
+                    <img src="<?php echo get_template_directory_uri() . "/img/wooman-day-mob.png";?>" alt="wooman-day-mob">
+                    <?php
+                } else {
+                    ?>
+                    <img src="<?php echo get_template_directory_uri() . "/img/wooman-day-pc.png";?>" alt="wooman-day-pc">
+                    <?php
+                }
+                ?>
+
+            </div>
+            <div class="banner-dark__container main-container">
+                <h1 class="banner-dark__title white">
+                    <?php
+                    echo $currentLang;
+                        if ($currentLang == "ua"){
+                            ?>
+                            -25% на план<br>
+                            стомотологічного<br>
+                            лікування
+                            <?php
+                        } else {
+                            ?>
+                            -25% на план<br>
+                            стоматологического<br>
+                            лечения
+                            <?php
+                        }
+                    ?>
+                </h1>
+                <div class="banner-dark__subtitle white">
+                    <?php
+                    if ($currentLang == "ua"){
+                        ?>
+                        до 8 березня
+                        <?php
+                    } else {
+                        ?>
+                        к 8 марта
+                        <?php
+                    }
+                    ?>
+
+                </div>
+                <div class="banner-dark__lnk button js-form">
+                    <span><?php the_field('tekst_na_knopke', 544)?></span>
+                </div>
+            </div>
+        </section>
+<!--        --><?php //get_template_part( 'template-parts/content', 'banner-main' ); ?>
         <?php get_template_part( 'template-parts/content', 'digits' ); ?>
         <?php get_template_part( 'template-parts/content', 'services-big' ); ?>
         <?php get_template_part( 'template-parts/content', 'services-animate' ); ?>
